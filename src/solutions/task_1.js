02ae0b1cbb3677be72dc27e0b05e34ff580672cd
Arijit Patra <ari.clicks@gmail.com>
2022-03-05 00:58:20 +0530

    task1 functionality done
    task1 functionality done

--- src/solutions/task_1.js
+++ src/solutions/task_1.js
@@ -1,3 +1,40 @@
 // Please implement your solution in this file
 
-export const prepareData = () => {};
+export const prepareData = (filterObject) => {
+  return (d) => {
+    const filteredByYear = d.filter(
+      (i) => i.launch_year === filterObject.year.toString()
+    );
+    const payLoadsFiltered = filteredByYear.filter((i) => {
+      const yesOrNo = [];
+      i.rocket.second_stage.payloads.forEach((j) => {
+        if (j.customers.some((s) => s.includes(filterObject.customerName))) {
+          yesOrNo.push(1);
+        }
+      });
+      return yesOrNo.length > 0 ? true : false;
+    });
+
+    const timeSorted = payLoadsFiltered.sort(function (x, y) {
+      return y.launch_date_unix - x.launch_date_unix;
+    });
+
+    const payloadCountSorted = timeSorted.sort(function (x, y) {
+      return (
+        y.rocket.second_stage.payloads.length -
+        x.rocket.second_stage.payloads.length
+      );
+    });
+
+    return payloadCountSorted.reduce((a, i) => {
+      return [
+        ...a,
+        {
+          flight_number: i.flight_number,
+          mission_name: i.mission_name,
+          payloads_count: i.rocket.second_stage.payloads.length,
+        },
+      ];
+    }, []);
+  };
+};
